1f88b8b6b35a2e91e7343c7eb3310d2592c39062
phuyc <lhphuc70@gmail.com>
2024-03-23 00:05:02 +0200

    cope

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,190 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+<div class="nav-wrapper">
+	<div class="nav">
+	  <div class="nav-left">
+		<img
+		  loading="lazy"
+		  src="https://cdn.builder.io/api/v1/image/assets/TEMP/4d073864689f553246ffb0c010b671e552607ad215af5664cc854e5e1ffa9b08?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		  class="nav-right"
+		  alt="streak"
+		/>
+		<div class="div-4">
+		  <div class="div-5">Le Hong Phuc</div>
+		  <div class="div-6">Lvl 2</div>
+		</div>
+	  </div>
+	  <img
+		loading="lazy"
+		src="https://cdn.builder.io/api/v1/image/assets/TEMP/61237ef15d61fc936671d3eebec32210e0b04f6b5c3ac837e3c265f4e187082f?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		class="img-2"
+	  />
+	</div>
+	<div class="div-7">
+	  <div class="div-8">
+		<img
+		  loading="lazy"
+		  src="https://cdn.builder.io/api/v1/image/assets/TEMP/e6c992e1e6558a655d2831c81e25ef005e09f46eeca32843cb20dbd6ea5b758c?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		  class="img-3"
+		/>
+		<div class="div-9">Food</div>
+	  </div>
+	  <img
+		loading="lazy"
+		src="https://cdn.builder.io/api/v1/image/assets/TEMP/3aa730881938fc622e149a1befd457dd53e3c33c286fde9145aba93020825265?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		class="img-4"
+	  />
+	  <div class="div-10">
+		<img
+		  loading="lazy"
+		  src="https://cdn.builder.io/api/v1/image/assets/TEMP/55870022bedde7e68c1925e6b735a0d501e395f0d33db195d396489c62bf3808?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		  class="img-5"
+		/>
+		<div class="div-11">Transport</div>
+	  </div>
+	  <img
+		loading="lazy"
+		src="https://cdn.builder.io/api/v1/image/assets/TEMP/9a2b1f762dc31ff27c97311b7d6a61fcf3d39bff22766a767a30d63f0e0591b9?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		class="img-6"
+	  />
+	  <div class="div-12">
+		<img
+		  loading="lazy"
+		  src="https://cdn.builder.io/api/v1/image/assets/TEMP/616bb87647d7e25131d686830e2b61f8c036dc8d511a3db4d875f2f702683d91?apiKey=82bc7e2fd9f24a258adc4e243fdb130a&"
+		  class="img-7"
+		/>
+		<div class="div-13">Checkpoints</div>
+	  </div>
+	</div>
+  </div>
+  <style>
+	.nav-wrapper {
+	  display: flex;
+	  flex-direction: column;
+	  color: #000;
+	  font-weight: 400;
+	  padding: 14px 10px 24px;
 	}
-
-	h1 {
-		width: 100%;
+	.nav {
+	  display: flex;
+	  width: 100%;
+	  gap: 20px;
+	  justify-content: space-between;
+	  padding: 0 1px;
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	.nav-left {
+	  display: flex;
+	  gap: 4px;
 	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.img {
+	  aspect-ratio: 1;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 54px;
 	}
-</style>
+	.div-4 {
+	  display: flex;
+	  flex-direction: column;
+	  margin: auto 0;
+	}
+	.div-5 {
+	  font: 20px Inter, sans-serif;
+	}
+	.div-6 {
+	  margin-top: 4px;
+	  font: 16px Inter, sans-serif;
+	}
+	.img-2 {
+	  aspect-ratio: 2.7;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 145px;
+	  max-width: 100%;
+	}
+	.div-7 {
+	  border-radius: 16px;
+	  box-shadow: 0px 4px 4px 0px #000;
+	  border-color: rgba(0, 0, 0, 1);
+	  border-style: solid;
+	  border-width: 1px;
+	  background-color: #fff;
+	  display: flex;
+	  margin-top: 579px;
+	  align-items: center;
+	  gap: 7px;
+	  font-size: 18px;
+	  white-space: nowrap;
+	  justify-content: space-between;
+	  padding: 0 22px 0 10px;
+	}
+	.div-8 {
+	  align-self: stretch;
+	  display: flex;
+	  gap: 4px;
+	  margin: auto 0;
+	}
+	.img-3 {
+	  aspect-ratio: 1.14;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 25px;
+	}
+	.div-9 {
+	  font-family: Inter, sans-serif;
+	  margin: auto 0;
+	}
+	.img-4 {
+	  aspect-ratio: 0.02;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 1px;
+	  stroke-width: 1px;
+	  stroke: #000;
+	  border-color: rgba(0, 0, 0, 1);
+	  border-style: solid;
+	  border-width: 1px;
+	  align-self: stretch;
+	}
+	.div-10 {
+	  align-self: stretch;
+	  display: flex;
+	  gap: 0px;
+	  margin: auto 0;
+	}
+	.img-5 {
+	  aspect-ratio: 1.14;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 25px;
+	}
+	.div-11 {
+	  font-family: Inter, sans-serif;
+	}
+	.img-6 {
+	  aspect-ratio: 0.02;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 1px;
+	  stroke-width: 1px;
+	  stroke: #000;
+	  border-color: rgba(0, 0, 0, 1);
+	  border-style: solid;
+	  border-width: 1px;
+	  align-self: stretch;
+	}
+	.div-12 {
+	  align-self: stretch;
+	  display: flex;
+	  gap: 0px;
+	  margin: auto 0;
+	}
+	.img-7 {
+	  aspect-ratio: 1;
+	  object-fit: auto;
+	  object-position: center;
+	  width: 25px;
+	}
+	.div-13 {
+	  font-family: Inter, sans-serif;
+	  flex-grow: 1;
+	  flex-basis: auto;
+	}
+  </style>
