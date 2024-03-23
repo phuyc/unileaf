0d317b6c988f81bd5b24e5d461fe5f54cf0fb800
phuyc <lhphuc70@gmail.com>
2024-03-24 00:46:45 +0200

    rm .firebase

--- .firebase/unileaf-3114/functions/manifest.js
+++ .firebase/unileaf-3114/functions/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png","fire.svg","fork.jpg","happy.svg","home.svg","leaderboard.svg","Menu.svg","profile.svg","robots.txt","tree.svg"]),
-	mimeTypes: {".png":"image/png",".svg":"image/svg+xml",".jpg":"image/jpeg",".txt":"text/plain"},
+	assets: new Set(["bus.svg","favicon.png","fire.svg","fork.jpg","happy.svg","home.svg","leaderboard.svg","map.svg","Menu.svg","profile.svg","quiz.svg","robots.txt","tree.svg","x.svg"]),
+	mimeTypes: {".svg":"image/svg+xml",".png":"image/png",".jpg":"image/jpeg",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.Bkup0aR0.js","app":"_app/immutable/entry/app.ksaJKq08.js","imports":["_app/immutable/entry/start.Bkup0aR0.js","_app/immutable/chunks/entry.CWGuXhzB.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/entry/app.ksaJKq08.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.CERtv3XG.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.DMPF7Nnx.js","app":"_app/immutable/entry/app.BwVTrZ52.js","imports":["_app/immutable/entry/start.DMPF7Nnx.js","_app/immutable/chunks/entry.BB3snuxn.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/entry/app.BwVTrZ52.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.snAqb81S.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .firebase/unileaf-3114/functions/nodes/1.js
+++ .firebase/unileaf-3114/functions/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BNMLL5HN.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.CERtv3XG.js","_app/immutable/chunks/entry.CWGuXhzB.js"];
+export const imports = ["_app/immutable/nodes/1.BXwjl54C.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.snAqb81S.js","_app/immutable/chunks/entry.BB3snuxn.js"];
 export const stylesheets = [];
 export const fonts = [];
