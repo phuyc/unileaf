9639260e17ad37842fe702ea37ca42468c552575
phuyc <lhphuc70@gmail.com>
2024-03-23 19:36:16 +0200

    resolve

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -2,6 +2,11 @@
     export let title;
     export let src;
     export let handleClick = () => {};
+
+    import Modal from './Modal.svelte';
+    let showModal = false;
+    
+    var correctFood=document.getElementById("foodOption1")    
 </script>
 
 <button class="task" on:click={handleClick}>
@@ -11,22 +16,23 @@
     </div>
 </button>
 
-<Modal bind:showModal>
-    <div id="foodModal" class="modal1">
-        <div class="container">
-            <div class="modal-content">
-                <h1>What type of food did you eat?</h1>
-                <br>
-                <input type="radio" id="foodOption1" name="food" value="vegFood">
-                <h2>Vegeterian</h2>
-                <br>
-                <input type="radio" id="foodOption2" name="food" value="nonVegFood">
-                <h2>Non vegeterian</h2>
+<button class="task" on:click={() => (showModal = true)}>
+    <Modal bind:showModal>
+        <div id="foodModal" class="modal1">
+            <div class="container">
+                <div class="modal-content">
+                    <h1>What type of food did you eat?</h1>
+                    <br>
+                    <input type="radio" id="foodOption1" name="food" value="vegFood">
+                    <h2>Vegeterian</h2>
+                    <br>
+                    <input type="radio" id="foodOption2" name="food" value="nonVegFood">
+                    <h2>Non vegeterian</h2>
+                </div>
             </div>
         </div>
-    </div>
-
-</Modal>
+    </Modal>
+</button>
 
 
 <style>
